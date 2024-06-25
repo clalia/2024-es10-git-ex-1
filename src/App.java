09956e6503c0dd8a5274ebe162f9122d94fee5e4
clalia <158627323+clalia@users.noreply.github.com>
2024-06-25 14:59:09 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        test();
+    }
+
+    public static void test() {
+        System.out.println("Hello World!");
     }
 }
+
